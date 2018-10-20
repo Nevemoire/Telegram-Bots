3af609404af88d824edbaea2e0d5523db325cced
Nevemoire <44291653+Nevemoire@users.noreply.github.com>
2018-10-20 18:47:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
 admin = ['391206263']
 
 promolist = ['nevemoire', 'tayson']
-
-token = '657899165:AAFIjnuSxW32yP9aePD2wplXsFIgKMxs0iE'
-
-provider_token = '410694247:TEST:1613378e-5aef-4526-89e5-9dd4c6f0463a'
