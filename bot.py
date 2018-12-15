b2dbbfb2772e7c5eb24728f0c05efbad4bd5e52b
Nevemoire <44291653+Nevemoire@users.noreply.github.com>
2018-12-15 21:48:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ def user_join(bot, update, user_data):
         bot.send_message(text=f'''Пользователь {name} (@{nick}) попытался наебать систему и использовать ник {mdkname}
 ID: {user}''', chat_id='@whoismdkadmins')
         cursor.execute("UPDATE users SET cheated = 1 WHERE id=%s", (user,))
-        con.commit()
+        conn.commit()
         
         return CHOOSING
     else:
