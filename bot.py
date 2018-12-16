0170b2f09c6273a7aaf2518bf9f3b5470c2add33
Nevemoire <44291653+Nevemoire@users.noreply.github.com>
2018-12-16 13:16:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -382,7 +382,7 @@ def main():
             PST:
                 [MessageHandler(Filters.text, custom_toppost, pass_user_data=True)],
             PRFL:
-                [CallbackQueryHandler(profile_action, pass_user_data=True),
+                [CallbackQueryHandler(profile_action, pass_user_data=True)],
             TOP:
                 [CallbackQueryHandler(top_users_action, pass_user_data=True)],
         },
