b4c22f09df90e0111d755eda2763d139c079423f
Nevemoire <44291653+Nevemoire@users.noreply.github.com>
2019-08-08 12:58:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,46 +6,29 @@
 # If you're still using version 11.1.0, please see the examples at
 # https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
 
-"""
-Basic example for a bot that can receive payment from user.
-"""
 
+"""
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic inline bot example. Applies different text transformations.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 import logging
-
-from telegram import (ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice)
-from telegram.ext import (Updater, CommandHandler, MessageHandler,
-                          Filters,
-                          ConversationHandler, CallbackQueryHandler,
-                          PreCheckoutQueryHandler, ShippingQueryHandler)
-from telegram.ext.dispatcher import run_async
-import psycopg2
-import config
+import random
 import os
-import datetime
-from importlib import reload
-
-conn = psycopg2.connect(dbname=os.environ['dbname'], user=os.environ['user'], password=os.environ['password'],
-                        host=os.environ['host'])
-
-cursor = conn.cursor()
-
-today = datetime.datetime.today()
-
-reply_keyboard = [['О боте 👾', 'О авторе 👨🏻‍💻'],
-                  ['Пример 💶', 'Контакты 📲'],
-                  ['Статистика 📊'],['Хочу такого бота 🚀']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+import actions
+from telegram.ext.dispatcher import run_async
+import uuid
+from uuid import uuid4
 
 
-(CHOOSING, TYPING_REPLY, PAYMENT) = range(3)
+from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 
-
-commands = (
-    'О боте 👾, О авторе 👨🏻‍💻, Пример 💶, Контакты 📲, Статистика 📊, Хочу такого бота 🚀')
-ignorelist = commands.split(', ')
-members = 'creator, administrator, member'
-memberslist = members.split(', ')
-back = 'Назад'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -54,247 +37,54 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+members = 'creator, administrator, member'
+memberslist = members.split(', ')
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Здарова! Все новости я выкладываю здесь: @rozbiynuki')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+@run_async
+def inlinequery(update, context):
+    """Handle the inline query."""
+    userid = update.inline_query.from_user.id
+    target = update.inline_query.query
+    name = update.inline_query.from_user.full_name
+    member = context.bot.get_chat_member('@rozbiynuki', userid)
+    if member.status in memberslist:
+        results = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="Їбемо " + update.inline_query.query,
+                input_message_content=InputTextMessageContent(
+                    message_text=f'{name} {random.choice(actions.action1)} {target} {random.choice(actions.action2)}'))]
+
+        update.inline_query.answer(results)
+
+    else:
+        results = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="Перед трахом підпишися на @razbiynuki",
+                input_message_content=InputTextMessageContent(
+                    message_text=
+                    'Чтобы трахать всё и вся, надо подписаться на наш ламповый канал! Жми: @rozbiynuki'))]
+
+        update.inline_query.answer(results)
+
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-
-def start(update, context):
-    name = update.message.from_user.full_name
-    update.message.reply_text(
-        f'Привет, {name}!')
-    nick = update.message.from_user.username
-    userid = update.message.from_user.id
-    context.user_data['usrid'] = userid
-    context.user_data['username'] = nick
-    context.user_data['name'] = name
-    update.message.reply_text('''Воспользуйся меню ниже для взаимодействия с ботом.
-    
-Не знаешь с чего начать? Возможности бота описаны в разделе "О боте".''', reply_markup=markup)
-    cursor.execute("SELECT id FROM users WHERE id=%s", (userid,))
-    result = "%s" % cursor.fetchone()
-    if result == "None":
-        cursor.execute("INSERT INTO users (nickname, namesurname, id, totalspent) VALUES (%s, %s, %s, 0)", (nick, name, userid))
-        conn.commit()
-    else:
-        pass
-
-    return CHOOSING
-
-
-@run_async  
-def about_bot(update, context):
-    update.message.reply_text("""*Возможности бота*
-    
-- Запись, считывание и хранение информации в базах данных.
-- Массовая рассылка сообщений пользователям.
-- Функция обратной связи. (Для тех, кто не хочет палить свой аккаунт tg. Пересылает сообщения пользователей в личку или на указанный канал.)
-- *Форматирование* _текста_ `разными` [способами](http://www.example.com/).
-    
-Доступные команды:
-/start - запустить/перезагрузить бота
-/photo - бот пришлёт фото
-/doc - бот пришлёт документ
-/stats - статистика бота
-
-Также, при заказе я учту и реализую все ваши пожелания.""", parse_mode='MARKDOWN')
-
-    return CHOOSING
-
-
-@run_async  
-def about_author(update, context):
-    update.message.reply_text("""*Привет!* Меня зовут Данил.
-Студент (но это не точно), пока ещё не миллиардер, начинающий разработчик (учу `python`) и front-end (`js/react`) девелопер.
-
-Несколько работ из моего портфолио:
-- https://poli-trade.com.ua - собран на Tilda.ws.
-- https://mjm-corp.ee или https://mjmcorp.esy.es - написан вручную с нуля, без помощи каких-либо конструкторов.
-- https://active-sp.ru (калькулятор, презентация, рекламные макеты и др.) - собран на WP.
-
-Также, ссылка на мой сайт:
-https://nevermore.red - собран на Tilda.ws, вскоре будет заново переписан по технологии [Single Page Application](http://www.codenet.ru/webmast/js/spa/), на основе Bootstrap 4 + React.js.
-
-Есть вопросы и/или предложения? Пиши мне: @daaetoya""",  parse_mode='MARKDOWN', disable_web_page_preview=True)
-
-    return CHOOSING
-
-
-@run_async  
-def contacts(update, context):
-    update.message.reply_text("""Telegram: @daaetoya
-Instagram: [daniel.nvmr](https://instagram.com/daniel.nvmr)""", parse_mode="MARKDOWN", disable_web_page_preview=True)
-
-    return CHOOSING
-  
-
-@run_async  
-def echo(update, context):
-    doc = update.message.document
-    update.message.reply_text(doc.file_id)
-    
-    return CHOOSING
- 
-
-@run_async  
-def order(update, context):
-    update.message.reply_text("Проконсультироваться и заказать бота можно у @daaetoya ;)")
-
-    return CHOOSING
-  
-
-@run_async  
-def photo(update, context):
-    avatar = 'https://images.pexels.com/photos/207962/pexels-photo-207962.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    context.bot.send_photo(update.message.chat_id, photo=avatar)
-
-    return CHOOSING
-
-@run_async  
-def doc(update, context):
-    doc = 'BQADAgAD3AMAApBwiEjvdRoQ7cnNDgI'
-    context.bot.send_document(update.message.chat_id, document=doc)
-    
-    return CHOOSING
-
-
-def custom_choice(update, context):
-    reply_keyboardz = [['Назад']]
-    state = ReplyKeyboardMarkup(reply_keyboardz, one_time_keyboard=True, resize_keyboard=True)
-    keyboard = [[InlineKeyboardButton("Базовый (1000р)", callback_data="1"),
-                 InlineKeyboardButton("Стандарт (2500р)", callback_data="2")],
-                [InlineKeyboardButton("Про (5000р)", callback_data="3"),
-                 InlineKeyboardButton("Презентация (Бесплатно)", callback_data="4")]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('''Супер 😎''', reply_markup=state)
-    update.message.reply_text('Выбери тариф 👇', reply_markup=reply_markup)
-
-    return TYPING_REPLY
-
-
-def received_information(update, context):
-    query = update.callback_query
-    context.user_data['choice'] = query.data
-    # text = update.message.text
-    keyboard = [[InlineKeyboardButton("Перейти к оплате", callback_data="Оплата")]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    try:
-        cursor.execute(
-            "SELECT tariff, price, patrons FROM betsdb WHERE id=%s", (query.data,))
-
-        context.bot.edit_message_text(text='''*Инструкция*
-        
-Оплата работает в тестовом режиме, не вводите данные настоящих карт!
-
-Чтобы провести тестовую оплату используйте данные ниже:
-*Карта*: 4242 4242 4242 4242
-*Действительна до*: любая дата, но не раньше сегодняшнего дня
-*CVV*: любое трёхзначное число
-
-👇''', chat_id=query.message.chat_id, message_id=query.message.message_id, parse_mode='MARKDOWN')
-
-        query.answer('Отличный выбор 😎')
-
-        update.effective_message.reply_text('''*Тариф:* %s
-*Цена:* %s рублей
-*Уже купили:* %s человек''' % cursor.fetchone(), parse_mode='MARKDOWN', reply_markup=reply_markup)
-    except:
-        update.effective_message.reply_text("*Ошибка!* Что-то пошло не так..", parse_mode='MARKDOWN')
-
-        return TYPING_REPLY
-
-    return PAYMENT
-
-
-def button(update, context):
-    IDS = context.user_data['choice']
-    chat_id = update.effective_message.chat_id
-    try:
-      cursor.execute("SELECT tariff FROM betsdb WHERE id=%s", (IDS,))
-      tariff = "%s" % cursor.fetchone()
-      title = tariff
-      description = "Бот для оплат 💳"
-      # select a payload just for you to recognize its the donation from your bot
-      payload = "Custom-Payload"
-      # In order to get a provider_token see https://core.telegram.org/bots/payments#getting-a-token
-      provider_token = os.environ['provider_token']
-      start_parameter = "test-payment"
-      currency = "RUB"
-      # price in dollars
-      cursor.execute("SELECT price FROM betsdb WHERE id=%s", (IDS,))
-      pricez = "%s" % cursor.fetchone()
-      price = int(pricez)
-      # price * 100 so as to include 2 d.p.
-      prices = [LabeledPrice(tariff, price * 100)]
-
-      # optionally pass need_name=True, need_phone_number=True,
-      # need_email=True, need_shipping_address=True, is_flexible=True
-      context.bot.sendInvoice(chat_id, title, description, payload,
-                          provider_token, start_parameter, currency, prices, photo_url='https://images.pexels.com/photos/207962/pexels-photo-207962.jpeg')
-
-      return CHOOSING
-    except:
-      update.effective_message.reply_text('''Бесплатные услуги оплачивать не нужно.
-
-*Ваш код*''')
-      
-      return CHOOSING
-      
-
-
-# after (optional) shipping, it's the pre-checkout
-def precheckout_callback(update, context):
-    query = update.pre_checkout_query
-    # check the payload, is this from your bot?
-    if query.invoice_payload != 'Custom-Payload':
-        # answer False pre_checkout_query
-        query.answer(ok=False, error_message="Something went wrong...")
-    else:
-        query.answer(ok=True)
-
-
-# finally, after contacting to the payment provider...
-def successful_payment_callback(update, context):
-    IDS = context.user_data['choice']
-    usrid = context.user_data['usrid']
-    name = context.user_data['name']
-    date = today.strftime("%d-%m-%Y %H:%M UTC")
-    cursor.execute("SELECT tariff FROM betsdb WHERE id=%s", (IDS,))
-    tariff = "%s" % cursor.fetchone()
-    # do something after successful receive of payment?
-    update.effective_message.reply_text('''Благодарим за проведение тестовой оплаты!
-Посмотреть как выглядят заказы: @orderspaymentstg''', reply_markup=markup)
-    cursor.execute("UPDATE betsdb SET patrons = patrons+1 WHERE id=%s", (IDS,))
-    cursor.execute("SELECT price FROM betsdb WHERE id=%s", (IDS,))
-    product_price = "%s" % cursor.fetchone()
-    tsprice = int(product_price)
-    cursor.execute("SELECT totalspent FROM users WHERE id=%s", (usrid,))
-    ts = "%s" % cursor.fetchone()
-    ts = int(ts) + int(tsprice)
-    cursor.execute("UPDATE users SET totalspent = %s WHERE id=%s", (str(ts), usrid))
-    conn.commit()
-    context.bot.send_message(
-        text=f'''Пользователь [{name}](tg://user?id={usrid}) оплатил *{tsprice}* рублей.
-*Тариф*: {tariff}.
-*Дата*: {date}''', chat_id='@orderspaymentstg', parse_mode='MARKDOWN')
-
-
-def get_back(update, context):
-    update.message.reply_text("Главное меню 👾", reply_markup=markup)
-
-    return CHOOSING
-
-
-def stats(update, context):
-    userid = context.user_data['usrid']
-    cursor.execute("SELECT COUNT(*) FROM users")
-    max_users = "%s" % cursor.fetchone()
-    cursor.execute("SELECT SUM(totalspent) FROM users")
-    max_earnings = "%s" % cursor.fetchone()
-    context.bot.send_message(text=f"""Кол-во пользователей: {max_users}
-Всего заработано: {max_earnings} рублей""", chat_id=userid)
-
-    return CHOOSING
 
 
 def main():
@@ -306,37 +96,12 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        allow_reentry=True,
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-        states={
-            CHOOSING: [MessageHandler(Filters.regex('^О боте 👾$'), about_bot),
-                       MessageHandler(Filters.regex('^О авторе 👨🏻‍💻$'), about_author),
-                       MessageHandler(Filters.regex('^Пример 💶$'), custom_choice),
-                       MessageHandler(Filters.regex('^Контакты 📲$'), contacts),
-                       MessageHandler(Filters.regex('^Статистика 📊$'), stats),
-                       MessageHandler(Filters.regex('^Хочу такого бота 🚀$'), order),
-                       CommandHandler('stats', stats),
-                       CommandHandler('photo', photo),
-                       CommandHandler('doc', doc),
-                       MessageHandler(Filters.document, echo)],
-
-            PAYMENT:    [CallbackQueryHandler(button)],
-
-            TYPING_REPLY: [CallbackQueryHandler(received_information)],
-        },
-
-        fallbacks=[MessageHandler(Filters.regex('^Назад$'), get_back)]
-    )
-
-    # Pre-checkout handler to final check
-    dp.add_handler(PreCheckoutQueryHandler(precheckout_callback))
-
-    # Success! Notify your user!
-    dp.add_handler(MessageHandler(Filters.successful_payment, successful_payment_callback))
-
-    dp.add_handler(conv_handler)
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(InlineQueryHandler(inlinequery))
 
     # log all errors
     dp.add_error_handler(error)
@@ -344,7 +109,7 @@ def main():
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
