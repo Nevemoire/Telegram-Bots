0b2974d2a4665386315b746729efa2d927aba8b5
Nevemoire <44291653+Nevemoire@users.noreply.github.com>
2020-01-28 23:12:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def adminctrl(update, context):
 @run_async
 def getId(update, context):
     update.message.reply_text('Чтобы поделиться данным чатом, убедитесь что он есть в нашей базе данных и вставьте текст ниже в поле ввода сообщения, затем нажмите на кнопку в всплывшем окне.')
-    update.message.text(f'@chattygrambot {update.effective_message.forward_from_chat.id}')
+    update.message.text(f'@chattygrambot {update.message.forward_from.id}')
 
 
 @run_async
